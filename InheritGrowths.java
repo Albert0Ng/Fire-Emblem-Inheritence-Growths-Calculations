ff8d26b4438afc7074886f71988ee09c2d127f7f
Albert0Ng <albert.l.ng@hotmail.com>
2017-09-27 19:57:01 +1000

    Update InheritGrowths.java

--- InheritGrowths.java
+++ InheritGrowths.java
@@ -8,6 +8,7 @@ public class InheritGrowths{
         static final int Lck = 40;
         static final int Def = 15;
         static final int Res = 35;
+        //support unit
     }
     public class Felicia{//Felicia's base Growths
         static final int HP = 40;
@@ -18,6 +19,7 @@ public class InheritGrowths{
         static final int Lck = 55;
         static final int Def = 15;
         static final int Res = 35;
+        //healer/support
     }
     public class Moz{/*Mozu's base Growths
         Renamed to Moz as program could not retrieve class variables with class name "Mozu"
@@ -31,6 +33,17 @@ public class InheritGrowths{
         static final int Def = 35;
         static final int Res = 30;
     }
+    public class Elise{
+        static final int HP = 30;
+        static final int Atk = 5;
+        static final int Mag = 65;
+        static final int Skl = 25;
+        static final int Spd = 55;
+        static final int Lck = 70;
+        static final int Def = 15;
+        static final int Res = 40;
+        //Glass cannon incarnate/healer
+    }
     public class Effie{//Effie's base Growths
         static final int HP = 35;
         static final int Atk = 60;
@@ -40,6 +53,7 @@ public class InheritGrowths{
         static final int Lck = 50;
         static final int Def = 35;
         static final int Res = 30;
+        //All-rounder Cannon/can take hits
     }
     public class Nyx{//Nyx's base Growths
         static final int HP = 30;
@@ -50,5 +64,148 @@ public class InheritGrowths{
         static final int Lck = 30;
         static final int Def = 15;
         static final int Res = 30;
+        //Poor man's Elise
+    }
+    public class Camilla{//Camilla's base growths
+        static final int HP = 40;
+        static final int Atk = 50;
+        static final int Mag = 25;
+        static final int Skl = 50;
+        static final int Spd = 55;
+        static final int Lck = 25;
+        static final int Def = 35;
+        static final int Res = 45;
+        //Broken
+    }
+    public class Selena{//Selena's base growths
+        static final int HP = 40;
+        static final int Atk = 30;
+        static final int Mag = 5;
+        static final int Skl = 25;
+        static final int Spd = 45;
+        static final int Lck = 30;
+        static final int Def = 45;
+        static final int Res = 30;
+        //Fast physical tank
+    }
+    public class Beruka{//Base growths for Beruka
+        static final int HP = 45;
+        static final int Atk = 30;
+        static final int Mag = 10;
+        static final int Skl = 55;
+        static final int Spd = 30;
+        static final int Lck = 45;
+        static final int Def = 40;
+        static final int Res = 25;
+        //Physical tank
+    }
+    public class Peri{//Base growths for Peri
+        static final int HP = 30;
+        static final int Atk = 50;
+        static final int Mag = 5;
+        static final int Skl = 30;
+        static final int Spd = 50;
+        static final int Lck = 35;
+        static final int Def = 25;
+        static final int Res = 45;
+        //Swift mage killer
+    }
+    public class Charlotte{//Charlotte's base growth
+        static final int HP = 65;
+        static final int Atk = 55;
+        static final int Mag = 0;
+        static final int Skl = 35;
+        static final int Spd = 50;
+        static final int Lck = 45;
+        static final int Def = 20;
+        static final int Res = 5;
+        //Physical glass cannon
+    }
+    public class Rinkah{//Rinkah's base growths
+        static final int HP = 20;
+        static final int Atk = 25;
+        static final int Mag = 15;
+        static final int Skl = 50;
+        static final int Spd = 45;
+        static final int Lck = 35;
+        static final int Def = 45;
+        static final int Res = 20;
+        //Physical tank
+    }
+    public class Sakura{//Sakura's base growths
+        static final int HP = 45;
+        static final int Atk = 30;
+        static final int Mag = 50;
+        static final int Skl = 40;
+        static final int Spd = 40;
+        static final int Lck = 55;
+        static final int Def = 30;
+        static final int Res = 20;
+        //Healer/support
+    }
+    public class Hana{//Hana's base growths
+        static final int HP = 25;
+        static final int Atk = 55;
+        static final int Mag = 10;
+        static final int Skl = 40;
+        static final int Spd = 55;
+        static final int Lck = 25;
+        static final int Def = 20;
+        static final int Res = 30;
+        //Speedmaster
+    }
+    public class Orochi{//Orochi's base growths
+        static final int HP = 35;
+        static final int Atk = 5;
+        static final int Mag = 65;
+        static final int Skl = 50;
+        static final int Spd = 15;
+        static final int Lck = 35;
+        static final int Def = 25;
+        static final int Res = 45;
+        //Magical glass cannon that can't double
+    }
+    public class Hinoka{//Hinoka's base growths
+        static final int HP = 45;
+        static final int Atk = 45;
+        static final int Mag = 15;
+        static final int Skl = 40;
+        static final int Spd = 45;
+        static final int Lck = 40;
+        static final int Def = 35;
+        static final int Res = 40;
+        //Mage killer/all-rounder
+    }
+    public class Setsuna{//Setsuna's base growths
+        static final int HP = 30;
+        static final int Atk = 20;
+        static final int Mag = 0;
+        static final int Skl = 30;
+        static final int Spd = 60;
+        static final int Lck = 30;
+        static final int Def = 15;
+        static final int Res = 40;
+        //Mage killer/dodge tank
+    }
+    public class Oboro{
+        static final int HP = 30;
+        static final int Atk = 40;
+        static final int Mag = 20;
+        static final int Skl = 40;
+        static final int Spd = 40;
+        static final int Lck = 40;
+        static final int Def = 40;
+        static final int Res = 40;
+        //All rounder/flexible unit/best waifu
+    }
+    public class Kagero{
+        static final int HP = 30;
+        static final int Atk = 65;
+        static final int Mag = 0;
+        static final int Skl = 20;
+        static final int Spd = 45;
+        static final int Lck = 30;
+        static final int Def = 25;
+        static final int Res = 40;
     }
 }
