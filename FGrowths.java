ae5baafddd21462aeb15e056f70e8fd30b2cf430
Albert0Ng <albert.l.ng@hotmail.com>
2017-09-28 18:46:12 +1000

    Added Father growths for later options

---
+++ FGrowths.java
@@ -0,0 +1,112 @@
+public class FGrowths{
+    public class Jakob{
+        static final int HP = 50;
+        static final int Str = 30;
+        static final int Mag = 15;
+        static final int Skl = 40;
+        static final int Spd = 35;
+        static final int Lck = 45;
+        static final int Def = 25;
+        static final int Res = 25;
+    }
+    public class Kaze{
+        static final int HP = 55;
+        static final int Str = 40;
+        static final int Mag = 0;
+        static final int Skl = 45;
+        static final int Spd = 65;
+        static final int Lck = 20;
+        static final int Def = 20;
+        static final int Res = 35;
+    }
+    public class Silas{
+        static final int HP = 40;
+        static final int Str = 45;
+        static final int Mag = 5;
+        static final int Skl = 50;
+        static final int Spd = 40;
+        static final int Lck = 40;
+        static final int Def = 40;
+        static final int Res = 25;
+    }
+    public class Subaki{
+        static final int HP = 55;
+        static final int Str = 30;
+        static final int Mag = 20;
+        static final int Skl = 50;
+        static final int Spd = 20;
+        static final int Lck = 25;
+        static final int Def = 45;
+        static final int Res = 5;
+    }
+    public class Saizo{
+        static final int HP = 40;
+        static final int Str = 50;
+        static final int Mag = 45;
+        static final int Skl = 60;
+        static final int Spd = 30;
+        static final int Lck = 55;
+        static final int Def = 45;
+        static final int Res = 10;
+    }
+    public class Azama{
+        static final int HP = 55;
+        static final int Str = 50;
+        static final int Mag = 20;
+        static final int Skl = 40;
+        static final int Spd = 45;
+        static final int Lck = 40;
+        static final int Def = 40;
+        static final int Res = 20;
+    }
+    public class Hayato{
+        static final int HP = 50;
+        static final int Str = 30;
+        static final int Mag = 40;
+        static final int Skl = 30;
+        static final int Spd = 45;
+        static final int Lck = 60;
+        static final int Def = 40;
+        static final int Res = 20;
+    }
+    public class Hinata{
+        static final int HP = 55;
+        static final int Str = 35;
+        static final int Mag = 0;
+        static final int Skl = 25;
+        static final int Spd = 15;
+        static final int Lck = 45;
+        static final int Def = 45;
+        static final int Res = 15;
+    }
+    public class Takumi{
+        static final int HP = 50;
+        static final int Str = 35;
+        static final int Mag = 0;
+        static final int Skl = 60;
+        static final int Spd = 40;
+        static final int Lck = 45;
+        static final int Def = 35;
+        static final int Res = 20;
+    }
+    public class Kaden{
+        static final int HP = 45;
+        static final int Str = 40;
+        static final int Mag = 10;
+        static final int Skl = 25;
+        static final int Spd = 45;
+        static final int Lck = 50;
+        static final int Def = 35;
+        static final int Res = 40;
+    }
+    public class Ryoma{
+        static final int HP = 50;
+        static final int Str = 45;
+        static final int Mag = 0;
+        static final int Skl = 50;
+        static final int Spd = 45;
+        static final int Lck = 40;
+        static final int Def = 35;
+        static final int Res = 25;
+    }
+}
