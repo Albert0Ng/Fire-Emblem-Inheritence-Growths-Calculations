fed7d9647376a238959a20213acd8feb34d8ce1b
Albert0Ng <albert.l.ng@hotmail.com>
2017-09-28 11:34:13 +1000

    Added Hoshido children as options

--- CalculateGrowths.java
+++ CalculateGrowths.java
@@ -30,6 +30,33 @@ public class CalculateGrowths{
     else if(child.equals("VELOURIA")){
         Growths.Velouria();
     }
+    else if(child.equals("IGNATIUS")){
+        Growths.Ignatius();
+    }
+    else if(child.equals("SHIRO")){
+        Growths.Shiro();
+    }
+    else if(child.equals("KIRAGI")){
+        Growths.Kiragi();
+    }
+    else if(child.equals("ASUGI")){
+        Growths.notGaius();
+    }
+    else if(child.equals("CAELDORI")){
+        Growths.notCordelia();
+    }
+    else if(child.equals("RHAJAT")){
+        Growths.notTharja();
+    }
+    else if(child.equals("HISAME")){
+        Growths.Hisame();
+    }
+    else if(child.equals("MITAMA")){
+        Growths.Mitama();
+    }
+    else if(child.equals("SELKIE")){
+        Growths.Selkie();
+    }
     else if(child.equals("STOP")){
         loop = 0;
     }
